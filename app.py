68996db08f15ad31597f3164481f7494eda2fe18
Satishachari <samileysatish@gmail.com>
2025-05-29 16:16:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@
 
 try:
     import streamlit as st
-    import seaborn as pd
-    import pandas as sns
+    import seaborn as sns
+    import pandas as pd
     import matplotlib.pyplot as plt
     import io
     import traceback
